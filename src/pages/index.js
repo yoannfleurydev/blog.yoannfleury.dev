eb23675c2eeb7a58905d21c37ab28307ca4f2005
Yoann Fleury <yoann.fleury@yahoo.com>
2019-05-24 08:26:17 +0200

    feat: change date format for blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,7 +60,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "MMMM DD, YYYY")
+            date(formatString: "DD/MM/YYYY")
             title
             description
           }
