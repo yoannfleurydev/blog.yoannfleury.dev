e68b52dc57a9d3a8bd663922b4bc2d249249cee7
Yoann Fleury <yoann.fleury@yahoo.com>
2019-05-24 08:21:58 +0200

    feat: improve config and package json

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,11 +57,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Blog de Yoann Fleury`,
+        short_name: `YoannFleuryDev`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#081D22`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
       },
