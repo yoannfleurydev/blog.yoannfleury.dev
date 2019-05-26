9dfa23254f3c60d07f09ecc3f6363996359d41fa
Yoann Fleury <yoann.fleury@yahoo.com>
2019-05-26 14:45:21 +0200

    feat: improve footer with social link and rss

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,8 +63,32 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Mis à disposition grâce à{` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <div style={{ float: "right" }}>
+            <a href="/rss.xml" target="_blank" rel="noopener noreferrer">
+              rss
+            </a>
+          </div>
+          <a
+            href="https://github.com/yoannfleurydev"
+            rel="noopener noreferrer"
+            target="_blank"
+            title="Lien vers la page github de Yoann Fleury"
+          >
+            github
+          </a>{" "}
+          &bull;{" "}
+          <a
+            href="https://mobile.twitter.com/yoannfleurydev"
+            rel="noopener noreferrer"
+            target="_blank"
+            title="Lien vers la page twitter de Yoann Fleury"
+          >
+            twitter
+          </a>
+          <p>
+            © {new Date().getFullYear()}, Mis à disposition grâce à{` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </p>
         </footer>
       </div>
     )
