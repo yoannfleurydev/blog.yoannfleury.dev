dfc86ef665cd243bf9a9c01f40682cb132e7ba30
Yoann Fleury <yoann.fleury@yahoo.com>
2019-09-08 11:53:31 +0200

    feat: add analytics to follow number of visit
    analytics is configured as simple as it should be

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-147409206-1',
       },
     },
     {
